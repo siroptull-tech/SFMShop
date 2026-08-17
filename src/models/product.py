51f7fcbcea25e11365550876915adc3ac1801007
Александр Горбунов~ <aleksandr-sarov@mail.ru>
2026-08-17 21:42:29 +0300

    Добавлена структура для моделей

---
+++ src/models/product.py
@@ -0,0 +1,36 @@
+
+from .exceptions import InsufficientStockError, ValidationError
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+    def set_price(self, price):
+        if price < 0:
+            raise ValidationError("Цена не может быть отрицательной")
+        self.price = price
+        
+    def sell(self, amount):
+        if amount > self.quantity:
+            raise InsufficientStockError(f"Товара недостаточно. На складе: {self.quantity}, требуется: {amount}")
+        self.quantity -= amount
+        
+    def get_total_price(self):
+            return self.price * self.quantity
+    
+    def __lt__(self, other):
+        if isinstance(other, Product):
+            return self.price < other.price
+    
+    def __eq__(self, value):
+        if isinstance(value, Product):
+            return self.price == value.price and self.name == value.name
+        return False
+        
+    def __str__(self):
+        return f"Товар: {self.name}, Цена: {self.price} руб., Количество: {self.quantity}"
+        
+    def __repr__(self):
+        return f"Product('{self.name}', {self.price}, {self.quantity})"
+    
